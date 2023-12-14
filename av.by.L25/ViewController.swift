71c0737288ee6e2cce49205aab59d45b707b3870
Ivan <colodiniv@yandex.ru>
2023-12-14 17:11:43 +0600

    add TabBar

--- av.by.L25/ViewController.swift
+++ av.by.L25/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class TabBarController: UITabBarController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let advertisementVC = AdvertisementViewController()
+        let advertisementNavController = UINavigationController(rootViewController: advertisementVC)
+        
+        let searchVC = UIViewController()
+        searchVC.view.backgroundColor = .white
+        searchVC.title = "Поиск"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        let subscriptionsVC = UIViewController()
+        subscriptionsVC.view.backgroundColor = .white
+        subscriptionsVC.title = "Подписки"
+        subscriptionsVC.tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 1)
+        
+        let addVC = UIViewController()
+        addVC.view.backgroundColor = .white
+        addVC.title = "Добавить"
+        addVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 2)
+        
+        let messagesVC = UIViewController()
+        messagesVC.view.backgroundColor = .white
+        messagesVC.title = "Сообщения"
+        messagesVC.tabBarItem = UITabBarItem(tabBarSystemItem: .history, tag: 3)
+        
+        let menuVC = UIViewController()
+        menuVC.view.backgroundColor = .white
+        menuVC.title = "Меню"
+        menuVC.tabBarItem = UITabBarItem(tabBarSystemItem: .more, tag: 4)
+        
+        viewControllers = [advertisementNavController, searchVC, subscriptionsVC, addVC, messagesVC, menuVC]
     }
-
-
 }
 
